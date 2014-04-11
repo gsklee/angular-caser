a265c4da7a4067dad1bb5c98b83d106ffcf1b3fe
Gias Kay Lee <balancetraveller+github@gmail.com>
2014-04-11 18:57:16 +0800

    Add snakification

--- object-key.js
+++ object-key.js
@@ -24,4 +24,8 @@ constant('ObjectKey', new function() {
   this.camelize = _deepTransform(function(string) {
     return string.replace(/[-_\s]+(.)?/g, function(match, $1) {return $1 ? $1.toUpperCase() : ''});
   });
+
+  this.snakify = _deepTransform(function(string) {
+    return string.replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
+  });
 }());
